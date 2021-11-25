ba8f707516e441cbdc8643aba864c051d3b32e62
Makrand <makrandshingare@gmail.com>
2021-11-25 23:45:38 +0530

    [makrand] Comapring the Tow lines

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -15,7 +15,7 @@ public class LineComparison {
         int y2=sc.nextInt();
         double x=Math.pow((x2-x1), 2);
         double y=Math.pow((y2-y1), 2);
-        double length = Math.floor(Math.sqrt(x+y));
+        Double length = Math.floor(Math.sqrt(x+y));
         System.out.println(length);
         System.out.println("Enter the Second  line co-ordinated");
         System.out.println("Enter the first (x1, y1) point");
@@ -29,13 +29,26 @@ public class LineComparison {
         int n2=sc.nextInt();
         double a=Math.pow((m2-m1), 2);
         double b=Math.pow((n2-n1), 2);
-        double length1 = Math.floor(Math.sqrt(a+b));
+        Double length1 = Math.floor(Math.sqrt(a+b));
 
-        if(length1==length){
-            System.out.println("the length of line is equal");
-        }
-        else
-            System.out.println("the length of line is not equal");
+//        if(length1==length){
+//            System.out.println("the length of line is equal");
+//        }
+//        else
+//            System.out.println("the length of line is not equal");
         System.out.println(length1);
+//
+//
+        int result=length.compareTo(length1);
+        if(result==0){
+            System.out.println("The lines are equals");
+        }
+        else if(result==1){
+            System.out.println("First line is grater than second");
+        }
+        else {
+            System.out.println("First line os grater then Second");
+        }
+
     }
 }
